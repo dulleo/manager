fd41b29b4d850a6adc0adb182cf62972d7138397
dulleo <dulleo@gmail.com>
2019-05-12 08:31:47 +0200

    Initial commit

---
+++ src/main/java/com/dl/manager/service/CommunityService.java
@@ -0,0 +1,23 @@
+package com.dl.manager.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.dl.manager.entity.Community;
+
+/**
+ * 
+ * @author duskol May 12, 2019
+ *
+ */
+@Service
+public class CommunityService implements CommunityServiceInterface {
+
+	@Override
+	public List<Community> getAllCommunities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
