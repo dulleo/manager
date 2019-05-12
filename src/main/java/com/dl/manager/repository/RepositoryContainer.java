ad4487fb04e259824e51eca2ea4892575109f7cb
dulleo <dulleo@gmail.com>
2019-05-12 08:33:32 +0200

    Initial commit

---
+++ src/main/java/com/dl/manager/repository/RepositoryContainer.java
@@ -0,0 +1,13 @@
+package com.dl.manager.repository;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 
+ * @author duskol May 12, 2019
+ *
+ */
+@Component
+public class RepositoryContainer {
+
+}
