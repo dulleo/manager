b66beec4f4007fdd46aa34ac3715e3b512e3dc5c
dulleo <dulleo@gmail.com>
2019-05-12 08:20:33 +0200

    properties added

--- src/main/java/com/dl/manager/entity/Community.java
+++ src/main/java/com/dl/manager/entity/Community.java
@@ -1,10 +1,67 @@
 package com.dl.manager.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
 /**
  * 
  * @author duskol May 12, 2019
  *
  */
+@Entity
+@Table(name="communities")
 public class Community {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Column(name="name", unique=true)
+	private String name;
+	
+	@NotBlank
+	@Column(name="identification_number", unique=true)
+	private String identificationNumber;
+	
+	@NotBlank
+	@Column(name="pib", unique=true)
+	private String pib;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getIdentificationNumber() {
+		return identificationNumber;
+	}
+
+	public void setIdentificationNumber(String identificationNumber) {
+		this.identificationNumber = identificationNumber;
+	}
+
+	public String getPib() {
+		return pib;
+	}
+
+	public void setPib(String pib) {
+		this.pib = pib;
+	}
 }
