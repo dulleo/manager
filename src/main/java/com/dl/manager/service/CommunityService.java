8dc6db66badc78ba315ccd20dfaa2c9cf7ffe50f
dulleo <dulleo@gmail.com>
2019-05-12 08:42:47 +0200

    getAllCommunities implemented

--- src/main/java/com/dl/manager/service/CommunityService.java
+++ src/main/java/com/dl/manager/service/CommunityService.java
@@ -2,9 +2,11 @@ package com.dl.manager.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dl.manager.entity.Community;
+import com.dl.manager.repository.RepositoryContainer;
 
 /**
  * 
@@ -13,11 +15,15 @@ import com.dl.manager.entity.Community;
  */
 @Service
 public class CommunityService implements CommunityServiceInterface {
+	
+	@Autowired
+	private RepositoryContainer repoContainer;
 
 	@Override
 	public List<Community> getAllCommunities() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return repoContainer.getCommunityRepo().findAll();
+		
 	}
 
 }
