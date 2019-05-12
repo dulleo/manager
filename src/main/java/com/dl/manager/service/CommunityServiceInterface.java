d5776b40465bea5940fdf0e7d86f248803be55c9
dulleo <dulleo@gmail.com>
2019-05-12 08:30:02 +0200

    getAllCommunities method added

--- src/main/java/com/dl/manager/service/CommunityServiceInterface.java
+++ src/main/java/com/dl/manager/service/CommunityServiceInterface.java
@@ -1,10 +1,16 @@
 package com.dl.manager.service;
 
+import java.util.List;
+
+import com.dl.manager.entity.Community;
+
 /**
  * 
  * @author duskol May 12, 2019
  *
  */
 public interface CommunityServiceInterface {
+
+	List<Community> getAllCommunities();
 
 }
