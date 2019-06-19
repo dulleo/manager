da1a1e901b6587a15b0ba8a5ab3be105e9a73c57
dulleo <dulleo@gmail.com>
2019-06-19 09:05:36 +0200

    getAllAccounts url changed

--- src/main/java/com/dl/manager/controller/AccountController.java
+++ src/main/java/com/dl/manager/controller/AccountController.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dl.manager.entity.BankAccount;
-import com.dl.manager.entity.Community;
+import com.dl.manager.exception.ResourceNotFoundException;
 import com.dl.manager.service.AccountServiceInterface;
 
 /**
@@ -20,7 +21,7 @@ import com.dl.manager.service.AccountServiceInterface;
  *
  */
 @RestController
-@RequestMapping(value="/manager/communities")
+@RequestMapping(value="/manager/")
 @CrossOrigin(origins = "http://localhost:4200")
 public class AccountController {
 	
@@ -28,9 +29,9 @@ public class AccountController {
 	private AccountServiceInterface service;
 	
 	@ResponseStatus(value = HttpStatus.OK)
-	@RequestMapping(value="/accounts", method= RequestMethod.GET)
-	public List<BankAccount> getAllAccounts() {
-		return service.getAllAccounts();
+	@RequestMapping(value="communities/{id}/accounts", method= RequestMethod.GET)
+	public List<BankAccount> getAllAccounts(@PathVariable Long id) throws ResourceNotFoundException {
+		return service.getAllAccounts(id);
 	}
 
 }
