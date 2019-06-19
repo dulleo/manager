50a8f9ba5e3f49cc4a7650a3bb34813eb54fed19
dulleo <dulleo@gmail.com>
2019-06-19 09:06:42 +0200

    getAllAccounts implemented

--- src/main/java/com/dl/manager/service/AccountService.java
+++ src/main/java/com/dl/manager/service/AccountService.java
@@ -2,19 +2,45 @@ package com.dl.manager.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.dl.manager.entity.BankAccount;
+import com.dl.manager.entity.Community;
+import com.dl.manager.exception.ResourceNotFoundException;
+import com.dl.manager.repository.RepositoryContainer;
 
 /**
  * 
  * @author duskol Jun 19, 2019
  *
  */
+@Service
 public class AccountService implements AccountServiceInterface {
+	
+	private final static String NOT_FOUND_MESSAGE = "Stambena zajednica sa id=%s nije pronadjena!";
+	
+	@Autowired
+	private RepositoryContainer repoContainer;
 
 	@Override
-	public List<BankAccount> getAllAccounts() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<BankAccount> getAllAccounts(Long id) throws ResourceNotFoundException {
+		
+		Community communityFromDb = getCommunityFromDb(id);
+		
+		return repoContainer.getAccountRepo().findByCommunityId(communityFromDb.getId());
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 * @throws ResourceNotFoundException
+	 */
+	private Community getCommunityFromDb(Long id) throws ResourceNotFoundException {
+		Community communityFromDb = repoContainer.getCommunityRepo().findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException(String.format(NOT_FOUND_MESSAGE, id)));
+		return communityFromDb;
 	}
 
 }
