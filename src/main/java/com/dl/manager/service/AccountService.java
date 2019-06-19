c54e014e9f8a69543012ffea7940d01febb4c843
dulleo <dulleo@gmail.com>
2019-06-19 08:51:03 +0200

    initial commit

---
+++ src/main/java/com/dl/manager/service/AccountService.java
@@ -0,0 +1,20 @@
+package com.dl.manager.service;
+
+import java.util.List;
+
+import com.dl.manager.entity.BankAccount;
+
+/**
+ * 
+ * @author duskol Jun 19, 2019
+ *
+ */
+public class AccountService implements AccountServiceInterface {
+
+	@Override
+	public List<BankAccount> getAllAccounts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
