ff3d60f2b33d58de311a1e9c5d336ea59eb94d59
dulleo <dulleo@gmail.com>
2019-06-20 00:55:55 +0200

    createAccount implemented

--- src/main/java/com/dl/manager/controller/AccountController.java
+++ src/main/java/com/dl/manager/controller/AccountController.java
@@ -2,10 +2,14 @@ package com.dl.manager.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -32,6 +36,12 @@ public class AccountController {
 	@RequestMapping(value="communities/{id}/accounts", method= RequestMethod.GET)
 	public List<BankAccount> getAllAccounts(@PathVariable Long id) throws ResourceNotFoundException {
 		return service.getAllAccounts(id);
+	}
+	
+	@ResponseStatus(value = HttpStatus.CREATED)
+	@RequestMapping(value="communities/{id}/accounts", method= RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public void createAccount(@PathVariable Long id, @Valid @RequestBody BankAccount account) throws ResourceNotFoundException {
+		service.createAccount(id, account);
 	}
 
 }
