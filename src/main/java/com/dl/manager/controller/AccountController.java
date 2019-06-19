229d76ffd33a747f737958f77168fd8b015f4d43
dulleo <dulleo@gmail.com>
2019-06-19 08:52:01 +0200

    getAllAccounts implemented

--- src/main/java/com/dl/manager/controller/AccountController.java
+++ src/main/java/com/dl/manager/controller/AccountController.java
@@ -1,12 +1,18 @@
 package com.dl.manager.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dl.manager.entity.BankAccount;
+import com.dl.manager.entity.Community;
 import com.dl.manager.service.AccountServiceInterface;
-import com.dl.manager.service.CommunityServiceInterface;
 
 /**
  * 
@@ -20,5 +26,11 @@ public class AccountController {
 	
 	@Autowired
 	private AccountServiceInterface service;
+	
+	@ResponseStatus(value = HttpStatus.OK)
+	@RequestMapping(value="/accounts", method= RequestMethod.GET)
+	public List<BankAccount> getAllAccounts() {
+		return service.getAllAccounts();
+	}
 
 }
