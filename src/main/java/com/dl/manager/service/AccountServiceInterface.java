23b659a4a39bab8ec542cd5b05663b6e03d7f1e6
dulleo <dulleo@gmail.com>
2019-06-19 08:51:32 +0200

    getAllAccounts added

--- src/main/java/com/dl/manager/service/AccountServiceInterface.java
+++ src/main/java/com/dl/manager/service/AccountServiceInterface.java
@@ -1,10 +1,16 @@
 package com.dl.manager.service;
 
+import java.util.List;
+
+import com.dl.manager.entity.BankAccount;
+
 /**
  * 
  * @author duskol Jun 19, 2019
  *
  */
 public interface AccountServiceInterface {
+
+	List<BankAccount> getAllAccounts();
 
 }
