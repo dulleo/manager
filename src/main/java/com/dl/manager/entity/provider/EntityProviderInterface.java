8bcff8f34d232a50419bab48772e85442c17a23c
dulleo <dulleo@gmail.com>
2019-06-22 00:16:51 +0200

    getCommunityFromDb added

--- src/main/java/com/dl/manager/entity/provider/EntityProviderInterface.java
+++ src/main/java/com/dl/manager/entity/provider/EntityProviderInterface.java
@@ -1,10 +1,14 @@
 package com.dl.manager.entity.provider;
 
+import com.dl.manager.entity.Community;
+
 /**
  * 
  * @author duskol Jun 22, 2019
  *
  */
 public interface EntityProviderInterface {
+	
+	Community getCommunityFromDb(Long id);
 
 }
