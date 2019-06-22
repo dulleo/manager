e1b515ce8feb42f1c74a333621110e9a95884e27
dulleo <dulleo@gmail.com>
2019-06-22 08:23:58 +0200

    getAllDoorways added

--- src/main/java/com/dl/manager/service/DoorwayServiceInterface.java
+++ src/main/java/com/dl/manager/service/DoorwayServiceInterface.java
@@ -1,10 +1,16 @@
 package com.dl.manager.service;
 
+import java.util.List;
+
+import com.dl.manager.entity.Doorway;
+
 /**
  * 
  * @author duskol Jun 22, 2019
  *
  */
 public interface DoorwayServiceInterface {
+
+	List<Doorway> getAllDoorways(Long id);
 
 }
