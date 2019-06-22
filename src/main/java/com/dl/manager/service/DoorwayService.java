073d21ee3f5a6404f357eb126b1535ff38556c2b
dulleo <dulleo@gmail.com>
2019-06-22 08:47:15 +0200

    createDoorway implemented

--- src/main/java/com/dl/manager/service/DoorwayService.java
+++ src/main/java/com/dl/manager/service/DoorwayService.java
@@ -4,20 +4,44 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import com.dl.manager.entity.Doorway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.dl.manager.entity.Community;
+import com.dl.manager.entity.Doorway;
+import com.dl.manager.entity.provider.EntityProviderInterface;
+import com.dl.manager.exception.ResourceNotFoundException;
+import com.dl.manager.repository.RepositoryContainer;
+
+/**
+ * 
+ * @author duskol Jun 22, 2019
+ *
+ */
+@Service
 public class DoorwayService implements DoorwayServiceInterface {
+	
+	@Autowired
+	private EntityProviderInterface entityProvider;
+
+	@Autowired
+	private RepositoryContainer repoContainer;
 
 	@Override
-	public List<Doorway> getAllDoorways(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Doorway> getAllDoorways(Long communityId) throws ResourceNotFoundException {
+		
+		Community communityFromDb = entityProvider.getCommunityFromDb(communityId);
+		return repoContainer.getDoorwayRepo().findByCommunityId(communityFromDb.getId());
+		
 	}
 
 	@Override
-	public void createDoorway(Long id, @Valid Doorway doorway) {
-		// TODO Auto-generated method stub
-
+	public void createDoorway(Long communityId, Doorway doorway) throws ResourceNotFoundException {
+		
+		Community communityFromDb = entityProvider.getCommunityFromDb(communityId);
+		doorway.setCommunity(communityFromDb);
+		repoContainer.getDoorwayRepo().save(doorway);
+		
 	}
 
 	@Override
