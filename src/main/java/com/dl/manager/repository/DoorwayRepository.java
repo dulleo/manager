34b694d731dac2782103de30a69ec314cb85d559
dulleo <dulleo@gmail.com>
2019-06-22 08:36:23 +0200

    method added

--- src/main/java/com/dl/manager/repository/DoorwayRepository.java
+++ src/main/java/com/dl/manager/repository/DoorwayRepository.java
@@ -1,10 +1,22 @@
 package com.dl.manager.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dl.manager.entity.Doorway;
+
 /**
  * 
  * @author duskol Jun 22, 2019
  *
  */
-public interface DoorwayRepository {
+@Repository
+public interface DoorwayRepository extends JpaRepository<Doorway, Long>{
+	
+	List<Doorway> findByCommunityId(Long id);
+
+	Doorway findByIdAndCommunityId(Long Id, Long communityid);
 
 }
