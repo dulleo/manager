8cc9f1e89e081afb1a14522d5c1eb5e722459eb9
dulleo <dulleo@gmail.com>
2019-06-22 08:19:56 +0200

    properties added

--- src/main/java/com/dl/manager/entity/Doorway.java
+++ src/main/java/com/dl/manager/entity/Doorway.java
@@ -1,10 +1,71 @@
 package com.dl.manager.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /**
  * 
  * @author duskol May 12, 2019
  *
  */
+@Entity
+@Table(name="doorways")
 public class Doorway {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Column(name="number")
+	private String number;
+	
+	@ManyToOne(fetch=FetchType.LAZY, optional = false)
+	@JoinColumn(name = "community_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIgnore
+	private Community community;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public Community getCommunity() {
+		return community;
+	}
+
+	public void setCommunity(Community community) {
+		this.community = community;
+	}
+
+	@Override
+	public String toString() {
+		return "Doorway [id=" + id + ", number=" + number + ", community=" + community + "]";
+	}
 
 }
