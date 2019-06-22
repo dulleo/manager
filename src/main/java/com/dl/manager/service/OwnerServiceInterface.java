e81512b7ff5bb28a26789eefc19ab533fc238bd9
dulleo <dulleo@gmail.com>
2019-06-22 19:00:42 +0200

    initial commit

---
+++ src/main/java/com/dl/manager/service/OwnerServiceInterface.java
@@ -0,0 +1,13 @@
+package com.dl.manager.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * @author duskol Jun 22, 2019
+ *
+ */
+@Service
+public interface OwnerServiceInterface {
+
+}
