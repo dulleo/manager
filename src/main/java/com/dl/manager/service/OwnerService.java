6ed72efb6290bc6a476bade5bc876b945f878cfa
dulleo <dulleo@gmail.com>
2019-06-22 19:14:50 +0200

    initial commit

---
+++ src/main/java/com/dl/manager/service/OwnerService.java
@@ -0,0 +1,41 @@
+package com.dl.manager.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.dl.manager.entity.Owner;
+
+/**
+ * 
+ * @author duskol Jun 22, 2019
+ *
+ */
+@Service
+public class OwnerService implements OwnerServiceInterface {
+
+	@Override
+	public List<Owner> getAllOwners(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createOwner(Long id, Owner owner) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateOwner(Long id, Long ownerId, Owner owner) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteOwner(Long id, Long ownerId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
