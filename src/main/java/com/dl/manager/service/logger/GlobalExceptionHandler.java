54c0269e6cfdfdd389186796dd499605fde246c2
dulleo <dulleo@gmail.com>
2019-06-22 08:07:38 +0200

    return ErrorResponse

--- src/main/java/com/dl/manager/service/logger/GlobalExceptionHandler.java
+++ src/main/java/com/dl/manager/service/logger/GlobalExceptionHandler.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.dl.manager.common.BackendResponse;
+import com.dl.manager.common.ErrorResponse;
+import com.dl.manager.exception.EntityValidationException;
 import com.dl.manager.exception.ResourceNotFoundException;
 
 import org.springframework.http.HttpStatus;
@@ -22,17 +23,24 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
-    public BackendResponse processError(ResourceNotFoundException e) {
-        //return new Response(false, e.getMessage());
-		return null;
+    public ErrorResponse processError(ResourceNotFoundException e) {
+		return new ErrorResponse(e.getMessage());
+		
+    }
+	
+	@ExceptionHandler(EntityValidationException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ResponseBody
+    public ErrorResponse processError(EntityValidationException e) {
+		return new ErrorResponse(e.getMessage());
+		
     }
 	
 	@ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
-    public BackendResponse processError(Exception e) {
-        //return new Response(false, e.getMessage());
-		return null;
+    public ErrorResponse processError(Exception e) {
+		return new ErrorResponse(e.getMessage());
     }
 
 
