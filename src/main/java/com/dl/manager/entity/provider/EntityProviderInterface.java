4d4913adbbe0154480e34bc803d66526959dcf03
dulleo <dulleo@gmail.com>
2019-06-22 08:08:33 +0200

    initial commit

--- src/main/java/com/dl/manager/entity/provider/EntityProviderInterface.java
+++ src/main/java/com/dl/manager/entity/provider/EntityProviderInterface.java
@@ -1,6 +1,9 @@
 package com.dl.manager.entity.provider;
 
+import com.dl.manager.entity.BankAccount;
 import com.dl.manager.entity.Community;
+import com.dl.manager.exception.EntityValidationException;
+import com.dl.manager.exception.ResourceNotFoundException;
 
 /**
  * 
@@ -9,6 +12,10 @@ import com.dl.manager.entity.Community;
  */
 public interface EntityProviderInterface {
 	
-	Community getCommunityFromDb(Long id);
+	Community getCommunityFromDb(Long id) throws ResourceNotFoundException;
+
+	BankAccount getAccountFromDb(Long communityId, Long accountId, BankAccount account) throws ResourceNotFoundException, EntityValidationException;
+
+	BankAccount getAccountFromDb(Long communityId, Long accountId) throws ResourceNotFoundException;
 
 }
