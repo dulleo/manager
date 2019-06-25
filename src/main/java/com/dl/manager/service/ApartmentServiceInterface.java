2e1dc52919a80d5b7179fb366e3237a0ca4729c9
dulleo <dulleo@gmail.com>
2019-06-25 21:52:14 +0200

    getAllApartments added

---
+++ src/main/java/com/dl/manager/service/ApartmentServiceInterface.java
@@ -0,0 +1,16 @@
+package com.dl.manager.service;
+
+import java.util.List;
+
+import com.dl.manager.entity.Apartment;
+
+/**
+ * 
+ * @author duskol Jun 23, 2019
+ *
+ */
+public interface ApartmentServiceInterface {
+
+	List<Apartment> getAllApartments();
+
+}
