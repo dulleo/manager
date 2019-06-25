b6ff496d1d658c55dd0608a21c86f9ac1a71bfbd
dulleo <dulleo@gmail.com>
2019-06-25 21:54:33 +0200

    owner methods added

--- src/main/java/com/dl/manager/entity/provider/EntityProviderInterface.java
+++ src/main/java/com/dl/manager/entity/provider/EntityProviderInterface.java
@@ -3,6 +3,7 @@ package com.dl.manager.entity.provider;
 import com.dl.manager.entity.BankAccount;
 import com.dl.manager.entity.Community;
 import com.dl.manager.entity.Doorway;
+import com.dl.manager.entity.Owner;
 import com.dl.manager.exception.EntityValidationException;
 import com.dl.manager.exception.ResourceNotFoundException;
 
@@ -22,5 +23,9 @@ public interface EntityProviderInterface {
 	Doorway getDoorwayFromDb(Long communityId, Long doorwayId, Doorway doorway) throws EntityValidationException, ResourceNotFoundException;
 	
 	Doorway getDoorwayFromDb(Long communityId, Long doorwayId) throws ResourceNotFoundException;
+
+	Owner getOwnerFromDb(Long communityId, Long ownerId, Owner owner) throws EntityValidationException, ResourceNotFoundException;
+	
+	Owner getOwnerFromDb(Long communityId, Long ownerId) throws ResourceNotFoundException;
 
 }
