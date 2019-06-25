f1698cdb3718da42c40417c3440ebfa4153bb6b9
dulleo <dulleo@gmail.com>
2019-06-25 21:50:30 +0200

    getAllApartments implemented

---
+++ src/main/java/com/dl/manager/controller/ApartmentController.java
@@ -0,0 +1,35 @@
+package com.dl.manager.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dl.manager.entity.Apartment;
+import com.dl.manager.service.ApartmentServiceInterface;
+
+/**
+ * 
+ * @author duskol Jun 23, 2019
+ *
+ */
+@RestController
+@RequestMapping(value="/manager")
+@CrossOrigin(origins = "http://localhost:4200")
+public class ApartmentController {
+	
+	@Autowired
+	private ApartmentServiceInterface service;
+	
+	@ResponseStatus(value = HttpStatus.OK)
+	@RequestMapping(value="/apartments", method= RequestMethod.GET)
+	public List<Apartment> getAllApartments() {
+		return service.getAllApartments();
+	}
+
+}
