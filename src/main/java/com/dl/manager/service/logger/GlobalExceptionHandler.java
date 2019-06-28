bce53b3b94795cd9b68c3c79726a70b3f86383ea
dulleo <dulleo@gmail.com>
2019-06-28 09:48:24 +0200

    DataIntegrityViolationException handler added

--- src/main/java/com/dl/manager/service/logger/GlobalExceptionHandler.java
+++ src/main/java/com/dl/manager/service/logger/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.dl.manager.service.logger;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,8 +10,12 @@ import com.dl.manager.common.ErrorResponse;
 import com.dl.manager.exception.EntityValidationException;
 import com.dl.manager.exception.ResourceNotFoundException;
 
-import org.springframework.http.HttpStatus;
+import java.sql.SQLIntegrityConstraintViolationException;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
 
 /**
  * 
@@ -20,21 +25,57 @@ import org.springframework.http.HttpStatus;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ResponseBody 
+    public ErrorResponse processError(DataIntegrityViolationException e) {
+		
+		String errorMessage = "";
+		
+		if(e.getCause().getCause() != null && e.getCause().getCause() instanceof SQLIntegrityConstraintViolationException) {
+			errorMessage = e.getCause().getCause().getMessage();
+		} else {
+			errorMessage = e.getMessage();
+		}
+		return new ErrorResponse(errorMessage);
+    }
+	
 	@ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ResponseBody
+    @ResponseBody 
     public ErrorResponse processError(ResourceNotFoundException e) {
 		return new ErrorResponse(e.getMessage());
-		
     }
 	
 	@ExceptionHandler(EntityValidationException.class)
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ResponseStatus(HttpStatus.CONFLICT)
     @ResponseBody
     public ErrorResponse processError(EntityValidationException e) {
 		return new ErrorResponse(e.getMessage());
-		
     }
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ResponseBody
+	public ErrorResponse processError(MethodArgumentNotValidException ex) {
+		
+		StringBuilder sb = new StringBuilder();
+		BindingResult bindingResult = ex.getBindingResult();
+		
+		bindingResult.getAllErrors().stream().forEach(e-> {
+			sb.append(e.getDefaultMessage()).append(";");
+		});
+		
+		String errorMessage = sb.toString().substring(0, sb.toString().length() - 1);
+		return new ErrorResponse(errorMessage);
+	}
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ResponseBody
+	public ErrorResponse handleBindingErrors(HttpMessageNotReadableException ex) {
+		return new ErrorResponse(ex.getMessage());
+	}
 	
 	@ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -42,7 +83,4 @@ public class GlobalExceptionHandler {
     public ErrorResponse processError(Exception e) {
 		return new ErrorResponse(e.getMessage());
     }
-
-
-
 }
