20b24853cf3616843dfcc87198cc1c642ee20395
dulleo <dulleo@gmail.com>
2019-06-28 09:38:07 +0200

    getAllApartments request url changed

--- src/main/java/com/dl/manager/controller/ApartmentController.java
+++ src/main/java/com/dl/manager/controller/ApartmentController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dl.manager.entity.Apartment;
+import com.dl.manager.exception.ResourceNotFoundException;
 import com.dl.manager.service.ApartmentServiceInterface;
 
 /**
@@ -27,9 +29,9 @@ public class ApartmentController {
 	private ApartmentServiceInterface service;
 	
 	@ResponseStatus(value = HttpStatus.OK)
-	@RequestMapping(value="/apartments", method= RequestMethod.GET)
-	public List<Apartment> getAllApartments() {
-		return service.getAllApartments();
+	@RequestMapping(value="communities/{id}/doorways/{doorwayId}/apartments", method= RequestMethod.GET)
+	public List<Apartment> getAllApartments(@PathVariable Long id, @PathVariable Long doorwayId) throws ResourceNotFoundException {
+		return service.getAllApartments(id, doorwayId);
 	}
 
 }
