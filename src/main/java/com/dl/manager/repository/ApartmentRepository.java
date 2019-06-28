26dce37d7c144486b25d149edbb61a9bbb4918bc
dulleo <dulleo@gmail.com>
2019-06-28 09:42:16 +0200

    findByDoorwayId added

--- src/main/java/com/dl/manager/repository/ApartmentRepository.java
+++ src/main/java/com/dl/manager/repository/ApartmentRepository.java
@@ -1,5 +1,7 @@
 package com.dl.manager.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,7 @@ import com.dl.manager.entity.Apartment;
  */
 @Repository
 public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
+
+	List<Apartment> findByDoorwayId(Long id);
 
 }
