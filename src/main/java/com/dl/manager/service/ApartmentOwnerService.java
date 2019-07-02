e3d34a950fd0bc56f9591e021fb53d340d388ef4
dulleo <dulleo@gmail.com>
2019-07-02 12:07:42 +0200

    initial commit

---
+++ src/main/java/com/dl/manager/service/ApartmentOwnerService.java
@@ -0,0 +1,20 @@
+package com.dl.manager.service;
+
+import java.util.List;
+
+import com.dl.manager.entity.dto.OwnerDTO;
+
+/**
+ * 
+ * @author duskol Jul 2, 2019
+ *
+ */
+public class ApartmentOwnerService implements ApartmentOwnerInterface {
+
+	@Override
+	public List<OwnerDTO> getOwnersForAccount(Long apartmentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
