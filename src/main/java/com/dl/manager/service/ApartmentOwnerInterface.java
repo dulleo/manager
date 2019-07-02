4642266e77488f6e5aae9c7d5d114ebca9396cab
dulleo <dulleo@gmail.com>
2019-07-02 12:05:47 +0200

    getOwnersForAccount added

--- src/main/java/com/dl/manager/service/ApartmentOwnerInterface.java
+++ src/main/java/com/dl/manager/service/ApartmentOwnerInterface.java
@@ -1,10 +1,16 @@
 package com.dl.manager.service;
 
+import java.util.List;
+
+import com.dl.manager.entity.dto.OwnerDTO;
+
 /**
  * 
  * @author duskol Jul 2, 2019
  *
  */
 public interface ApartmentOwnerInterface {
+
+	List<OwnerDTO> getOwnersForAccount(Long apartmentId);
 
 }
